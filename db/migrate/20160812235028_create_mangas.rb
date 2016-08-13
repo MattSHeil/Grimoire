7a1c3de86565e887b75a360e83088360f0e8cd69
Mnatherr <matheus.heil001@mymdc.net>
2016-08-12 20:06:56 -0400

    Migrated db, following columns added title, link_to_page, total_chapters, last_chapter and posted_date

--- db/migrate/20160812235028_create_mangas.rb
+++ db/migrate/20160812235028_create_mangas.rb
@@ -1,7 +1,12 @@
 class CreateMangas < ActiveRecord::Migration[5.0]
   def change
     create_table :mangas do |t|
-
+      t.string :title
+      t.string :link_to_page
+      t.integer :total_chapters
+      t.integer :last_chapter
+      t.string :posted_date
+    	
       t.timestamps
     end
   end
