fb44b2af3326acc9856b8cf056b9e40ecf8ff132
Mnatherr <matheus.heil001@mymdc.net>
2016-09-02 18:47:31 -0400

    Generated artist and author models, only name present on both

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160824213712) do
+ActiveRecord::Schema.define(version: 20160902224426) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "labeled_mangas", force: :cascade do |t|
     t.integer  "manga_id"
