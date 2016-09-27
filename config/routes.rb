b32fd8ce8400fd8fd7d26bba8eb671647465f28a
Mnatherr <matheus.heil001@mymdc.net>
2016-09-27 16:30:11 -0400

    New routes to handle separete controllers for manga /views and /api, routes will need refactoring for search actions,

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,18 @@ Rails.application.routes.draw do
   root to: "static_pages#home"
   devise_for :users
   get '/user/:id', to: 'users#show', as: :user_page
-  resources :mangas, except: [:new, :edit]
-  get '/search', to: 'mangas#search', as: :search_manga
+  # resources :mangas, except: [:new, :edit]
+  get '/search', to: 'api/mangas#search', as: :search_manga
+
+  namespace :api do 
+  	resources :mangas
+  end
+
+  scope module: "views" do 
+  	resources :mangas 
+  end
 
   # API request routes
-  get "/mangas/search/:keyword", to: "mangas#searchLabel"
-  get "/mangas/list/:keyword", to: "mangas#searchName"
+  get "/mangas/search/:keyword", to: "api/mangas#searchLabel"
+  get "/mangas/list/:keyword", to: "api/mangas#searchName"
 end
