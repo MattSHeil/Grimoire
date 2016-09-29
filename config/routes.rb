66853da1f73a6b11f4687fc8be36a84e6985522e
Mnatherr <matheus.heil001@mymdc.net>
2016-09-29 15:30:57 -0400

    Changed couple routes for searching and regarding views and api requests for mangas

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,27 @@
 Rails.application.routes.draw do
-  root to: "static_pages#home"
+  root to: 'static_pages#home'
   devise_for :users
   get '/user/:id', to: 'users#show', as: :user_page
-  # resources :mangas, except: [:new, :edit]
-  get '/search', to: 'api/mangas#search', as: :search_manga
+  
+  # API request routes
+  get '/advanced', to: 'api/mangas#advanced', as: :api_advanced_search_manga
+
+  # Route to be used if api request is necessary, if make it into gem for example
+  # get '/search/:keyword', to: "api/mangas#search", as: :api_search_manga
+
+  # add and delete routes for users mangas
+  post '/mangas/:id/add', to: 'user_mangas#add_manga', as: :add_manga_to_user
+  delete '/mangas/:id/delete', to: 'user_mangas#delete_manga', as: :delete_manga_from_user
 
   namespace :api do 
-  	resources :mangas
+    resources :mangas
   end
 
   scope module: "views" do 
-  	resources :mangas 
+    resources :mangas 
   end
+  
+  # Search MangaRoute
+  get '/search', to: 'views/mangas#search', as: :views_search_manga
 
-  # API request routes
-  get "/mangas/search/:keyword", to: "api/mangas#searchLabel"
-  get "/mangas/list/:keyword", to: "api/mangas#searchName"
 end
