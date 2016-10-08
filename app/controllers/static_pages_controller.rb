514aff33710d7c1ff1dd972c55e30f2c46d33ca3
Mnatherr <matheus.heil001@mymdc.net>
2016-10-08 19:40:04 -0400

    Trying to debug deployment

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
 	def home
-		@manga = Manga.all
+		@manga = Manga.new
 		if params[:search]
 			@manga = Manga.search(params[:search]).order("updated_at DESC")
 		else
@@ -8,19 +8,5 @@ class StaticPagesController < ApplicationController
 		end
 
 		@latests = Manga.latests
-
-		# begin
-		# 	open(.manga_img.cover_img_url)
-		# rescue => error
-		# 	if error
-		# 		@img = "https://dummyimage.com/200x310"
-		# 	else
-		# 		@img = @manga.manga_img.cover_img_url
-		# 	end
-		# end
-
-		# if user_signed_in?
-		# 	redirect_to "/user/#{current_user.id}"
-		# end
 	end
 end
